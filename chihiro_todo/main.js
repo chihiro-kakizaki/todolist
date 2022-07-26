84350f7433b7e3576b4dc6534bd3a3dfa985f773
chihiro-kakizaki <kkzkchr@gmail.com>
2022-07-27 05:37:28 +0900

    担当者追加機能

--- chihiro_todo/main.js
+++ chihiro_todo/main.js
@@ -2,15 +2,24 @@ const taskSubmitButton = document.querySelector('#task_submit');
 const todoArea = document.querySelector('#todo_area');
 const doingArea = document.querySelector('#doing_area');
 const doneArea = document.querySelector('#done_area');
+const personArea = document.querySelector('#person_area');
 
 
 // taskをデータとして管理する変数
 let taskList = [];
 
+//担当者をデータとして管理する変数
+const personList = [{ id: "1", name: "柿崎"}, {id: "2", name: "柴田"}, {id: "3", name: "黒澤"}];
+
+//プルダウンで選択された担当者と一致するidをpersonListから探し出し、担当者名で返却する関数
+const getPersonNameById = (personId) => {
+    return personList.find(person => person.id === personId).name;
+}
+
 // タスクを追加する関数
-const addTask = (newTask) => {
+const addTask = function(newTask, selectPerson) {
     if(newTask === '') return; // 何も入力されてなかったら処理終了
-    taskList.push({ title:newTask, status:0}); // 新しいタスクを配列の最後に追加
+    taskList.push({ title:newTask, status:0, personId: selectPerson}); // 新しいタスクを配列の最後に追加
     renderTaskList(); // htmlとして表示する
 }
 
@@ -42,21 +51,21 @@ const doneTask = (index) => {
 const renderTaskList = () => {
     todoArea.innerHTML = taskList.map((task, i) => {
         if (task.status === 0) {
-            return `<li>${task.title}<button onclick="deleteTask(${i})">delete</button><div><button onclick="doingTask(${i})">doing</button><button onclick="doneTask(${i})">done</button></li></div>`//各taskに対してli要素の配列を作成
+            return `<li>${task.title}<button onclick="deleteTask(${i})">delete</button><div><button onclick="doingTask(${i})">doing</button><button onclick="doneTask(${i})">done</button><p>担当者:${getPersonNameById(task.personId)}</p></li></div>`//各taskに対してli要素の配列を作成
         }
         return "";
     }).join('');
 
     doingArea.innerHTML = taskList.map((task, i) => {
         if (task.status === 1) {
-            return `<li>${task.title}<button onclick="deleteTask(${i})">delete</button><div><button onclick="todoTask(${i})">todo</button><button onclick="doneTask(${i})">done</button></li></div>`
+            return `<li>${task.title}<button onclick="deleteTask(${i})">delete</button><div><button onclick="todoTask(${i})">todo</button><button onclick="doneTask(${i})">done</button><p>担当者:${getPersonNameById(task.personId)}</p></li></div>`
         }
         return "";            
     }).join('');
 
     doneArea .innerHTML = taskList.map((task, i) => {
         if (task.status === 2) {
-            return `<li>${task.title}<button onclick="deleteTask(${i})">delete</button><div><button onclick="todoTask(${i})">todo</button><button onclick="doingTask(${i})">doing</button></li></div>`
+            return `<li>${task.title}<button onclick="deleteTask(${i})">delete</button><div><button onclick="todoTask(${i})">todo</button><button onclick="doingTask(${i})">doing</button><p>担当者:${getPersonNameById(task.personId)}</p></li></div>`
         }
         return "";
     }).join('');            
@@ -64,6 +73,7 @@ const renderTaskList = () => {
 
 taskSubmitButton.addEventListener('click', function() {
     const newTask = document.querySelector('#task_value');
-    addTask(newTask.value);
+    const selectPerson = document.querySelector('#person_select');
+    addTask(newTask.value, selectPerson.value);
     newTask.value = '';
 });
